f8e0457cf9c199c97162e69770243a9bd8449112
Piotr Kwietniewski <piotr.kwietniewski@aspekt.net.pl>
2024-02-09 15:12:27 +0100

    Default App.js changed to App.tsx

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native' 
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Home from './Screens/Home'
+
+const Stack = createStackNavigator()
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'transparent',
+  },
+}
+
+export default function App() {
+  return (
+    <>
+      <StatusBar/>
+      <NavigationContainer theme={theme}>
+        <Stack.Navigator
+          screenOptions={{ headerShown: false }}
+          initialRouteName="Home"
+        >
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+
+
