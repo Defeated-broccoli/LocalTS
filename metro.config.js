d36962296ec1a74cb15f979430d55c300dce1a98
Defeated-broccoli <piotrkwietniewski12@gmail.com>
2024-02-22 00:59:33 +0100

    packages

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,11 @@
-const { getDefaultConfig } = require('expo/metro-config')
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
 
-const defaultConfig = getDefaultConfig(__dirname)
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {}
 
-defaultConfig.resolver.assetExts.push('db')
-
-module.exports = defaultConfig
+module.exports = mergeConfig(getDefaultConfig(__dirname), config)
